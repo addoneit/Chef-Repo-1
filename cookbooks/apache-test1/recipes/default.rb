0eeb915a58fcb5794520459f804446db71cb94a0
jeanpauln <jpnavarro@addoneit.com>
2014-08-14 17:07:40 +0200

    Test Changes

--- cookbooks/apache-test1/recipes/default.rb
+++ cookbooks/apache-test1/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: apache-test1
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Copyright 2014, AddOne IT Services Ltd.
 #
 # All rights reserved - Do Not Redistribute
 #
@@ -10,18 +10,36 @@
 #This Recipe will install Apache, start the service on reboot and write homepage
 #This is done following the Chef Fundamentals Webinar Module 3 (https://www.youtube.com/watch?v=71Cq4bCxgDk)
 
+package_name="apache2"
+service_name="apache2"
+document_root="/var/www"
+
+if node["platform"] == "centos"
+	package_name="httpd"
+	service_name="httpd"
+	document_root="/var/www/html"
+end
+
 #Install Apache
-package "apache2" do
+package package_name do
 	action :install
 end
 
 #Start Apache service on start/ reboot
-service "apache2" do
+service service_name do
 	action [:start, :enable]
 end
 
-#write homepage
-Cookbook_file "/var/www/index.html" do
-	source "index.html"
+# #write homepage
+# Cookbook_file "#{document_root}/index.html" do
+# 	source "index.html"
+# 	mode "0644"
+# end
+
+
+
+template "#{document_root}/index.html" do
+	source "index.html.erb"
 	mode "0644"
 end
+
